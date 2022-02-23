0dca13b0c19facd1b094de9f110c7762b36cbf73
mcmontag <54485715+mcmontag@users.noreply.github.com>
2022-02-23 13:08:23 -0500

    Create messaging.js

---
+++ auto-set-lineup/messaging.js
@@ -0,0 +1,25 @@
+const teamRegex = /team[0-9]+/
+
+async function fetchAsync (url) {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
+}
+
+function getCurrentUrl() {
+  return window.location.pathname
+}
+
+function getTeamId() {
+  let teamIdMatchArray = getCurrentUrl().match(teamRegex)
+  if teamIdMatchArray.length > 0 {
+    console.log(teamIdMatchArray[1].match(/[0-9]+/)[1])
+    return teamIdMatchArray[1].match(/[0-9]+/)[1]
+  } else {
+    return 0
+  }
+}
+
+function askToSetLineup() {
+  let url = getCurrentUrl()
+}
